56a21f233aaf07ca033f844caef348442f3ee8ba
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-10-20 21:08:33 -0400

    Testing workflow

--- mapd-core-cpu/build.sh
+++ mapd-core-cpu/build.sh
@@ -16,20 +16,20 @@ if [ $(uname) == Darwin ]; then
   export LDFLAGS="-L$PREFIX/lib $LDFLAGS -Wl,-rpath,$PREFIX/lib"
 fi
 
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_BUILD_TYPE=release \
-    -DENABLE_CUDA=off \
-    -DMAPD_IMMERSE_DOWNLOAD=off \
-    -DMAPD_DOCS_DOWNLOAD=off \
-    -DPREFER_STATIC_LIBS=off \
-    -DENABLE_AWS_S3=off \
-    -DENABLE_TESTS=on  \
-    -DCMAKE_C_COMPILER=$CC \
-    -DCMAKE_CXX_COMPILER=$CXX ..
-
-make ParserFiles
-make
-make install
+#cmake \
+#    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+#    -DCMAKE_BUILD_TYPE=release \
+#    -DENABLE_CUDA=off \
+#    -DMAPD_IMMERSE_DOWNLOAD=off \
+#    -DMAPD_DOCS_DOWNLOAD=off \
+#    -DPREFER_STATIC_LIBS=off \
+#    -DENABLE_AWS_S3=off \
+#    -DENABLE_TESTS=on  \
+#    -DCMAKE_C_COMPILER=$CC \
+#    -DCMAKE_CXX_COMPILER=$CXX ..
+#
+#make ParserFiles
+#make
+#make install
 
 cp -R $SRC_DIR/systemd $PREFIX
